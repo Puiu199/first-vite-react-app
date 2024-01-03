ec29d2c730f230450de87b8b4ebb5b5671947ff6
Puiu Dumitru99 <puiudumitru99@gmail.com>
2024-01-03 13:53:44 +0200

    first commit

---
+++ src/components/ElementList.jsx
@@ -0,0 +1,9 @@
+export default function ElementList(props){
+    return(
+        <li>
+            <strong>{props.title}</strong>
+            <p>{props.content} </p>
+        </li>
+    )
+
+}
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export let date=[
+    {
+        title:"Primul Element",
+        content:"Date despre primul element"
+    },
+    {
+        title:"Al doilea  Element",
+        content:"Date despre al doilea element"
+    },
+    {
+        title:"Al treilea  Element",
+        content:"Date despre al treilea element"
+    }
+]

85534e06606b49fa0142c333fa60d7b35596ad3d
Puiu Dumitru99 <puiudumitru99@gmail.com>
2024-01-09 15:45:57 +0200

    Done Frist etap

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
-import ElementList from "./components/ElementList"
-import Header from "./header/Header"
-import {date} from './data'
+import ElementList from "./components/ElementList";
+import Header from "./header/Header";
+import { date } from "./data";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-export default function App(){
-  return(
+export default function App() {
+  return (
     <div>
-      <Header>
-
-      </Header>
-    <main>
-     
-
-    </main>
-    <footer>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">Item</li>
-        <li className="list-group-item">Item</li>
-        <li className="list-group-item">Item</li>
-        <li className="list-group-item">Item</li>
-        <li className="list-group-item">Item</li>
-      </ul>
-      
-    </footer>
+      <Header></Header>
+      <main className="min-vh-100"></main>
+      <footer></footer>
     </div>
-    
-  )
+  );
 }

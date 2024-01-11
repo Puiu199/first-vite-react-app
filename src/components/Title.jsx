1b0d6471b0775e9a52f14205ae4a6018eb8eb485
Puiu Dumitru99 <puiudumitru99@gmail.com>
2024-01-11 16:22:53 +0200

    Done Secont stap

---
+++ src/components/Title.jsx
@@ -0,0 +1,4 @@
+import "../components/Components.css";
+export default function Title({ children }) {
+  return <span className="Title">{children}</span>;
+}
---
+++ src/components/YellowT.jsx
@@ -0,0 +1,4 @@
+import "./Components.css";
+export default function YellowT({ children }) {
+  return <span className="yellowT">{children}</span>;
+}

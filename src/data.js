11192ece19250d6b45ccc53a5fc2b3153e152895
Puiu Dumitru99 <puiudumitru99@gmail.com>
2024-01-16 08:49:49 +0200

    Revert "Merge pull request #6 from Puiu199/prod"
    This reverts commit 71d40af4bf36e47ee6903d50a19f3d6cfbb68a9b, reversing
    changes made to 103f48ae7fa87b61c56abb4721c3dc8f2642ebf7.

--- src/data.js
+++ src/data.js
@@ -1,6 +1,5 @@
 export let date = [
   {
-    id: 1,
     bgImg: "First.png",
     title: "Озеро возле гор",
     content: "романтическое приключение",
@@ -8,7 +7,6 @@ export let date = [
     valute: "$",
   },
   {
-    id: 2,
     bgImg: "Second.png",
     title: "Ночь в горах",
     content: "в компании друзей",
@@ -16,15 +14,6 @@ export let date = [
     valute: "$",
   },
   {
-    id: 3,
-    bgImg: "Three.png",
-    title: "Йога в горах",
-    content: "для тех, кто забоится о себе",
-    price: "230",
-    valute: "$",
-  },
-  {
-    id: 4,
     bgImg: "Three.png",
     title: "Йога в горах",
     content: "для тех, кто забоится о себе",
--- src/footer/Footer.jsx
+++
@@ -1,4 +0,0 @@
-import "../footer/Footer.css";
-export default function Footer({ children }) {
-  return <footer>{children}</footer>;
-}
